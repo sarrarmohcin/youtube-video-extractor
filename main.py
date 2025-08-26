2b62439336382cda85614ec4b5fabbb960bdd3b7
sarrarmohcin <sarrar.mohcin@gmail.com>
2025-08-26 13:11:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from channel import YoutubeChannel 
 from videos import YoutubeVideos
 import asyncio
+
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is not a positive integer" % value)
+    return ivalue
+
+parser = argparse.ArgumentParser(description='Process some inputs.')
+
+parser.add_argument(
+    "-c", "--channel",
+    type=str,
+    required=True,
+    help="Youtube channel ID (required)"
+)
+
+# Optional limit argument with -lm and --limit
+parser.add_argument(
+    "-l", "--limit",
+    type=positive_int,
+    default=10,
+    help="Limit of videos (default: 10)"
+)
+args = parser.parse_args()
+
 data = []
 
-channelExtractor = YoutubeChannel(channel_id = "CHANNEL_ID", limit = 10)
+channelExtractor = YoutubeChannel(channel_id = args.channel, limit = args.limit)
 channelExtractor.start()
 videos = channelExtractor.videos
 
